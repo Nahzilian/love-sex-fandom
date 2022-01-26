b3f6597059f6d02c9f0fdd78d0dd9dfc9aca4fe3
Nahzilian <nahz3112@gmail.com>
2022-01-26 15:36:59 -0500

    Update on layout and songs

--- script/index.js
+++ script/index.js
@@ -3,15 +3,45 @@ let track_index = 0;
 let audio = [
     {
         file: new Audio('./assets/songs/track1.mp3'),
-        cover: "./assets/songs/blue.jpg",
-        name: "Entertain me",
-        artist: "Ylona Garcia"
+        cover: "./assets/songs/cover1.jpeg",
+        name: "Make me feel",
+        artist: "Janelle Monae"
     },
     {
         file: new Audio('./assets/songs/track2.mp3'),
-        cover: "./assets/songs/blue.jpg",
-        name: "Put Your Records On",
-        artist: "Ritt Momney",
+        cover: "./assets/songs/cover2.jpg",
+        name: "Answer: Love Myself",
+        artist: "BTS"
+    },
+    {
+        file: new Audio('./assets/songs/track3.mp3'),
+        cover: "./assets/songs/cover3.jpg",
+        name: "Volcanic Love",
+        artist: "The Aces"
+    },
+    {
+        file: new Audio('./assets/songs/track4.mp3'),
+        cover: "./assets/songs/cover4.jpeg",
+        name: "Little Things",
+        artist: "One Direction"
+    },
+    {
+        file: new Audio('./assets/songs/track5.mp3'),
+        cover: "./assets/songs/cover5.jpg",
+        name: "Favourite Crime",
+        artist: "Olivia Rodrigo"
+    },
+    {
+        file: new Audio('./assets/songs/track6.mp3'),
+        cover: "./assets/songs/cover6.jpeg",
+        name: "Friday I'm in Love",
+        artist: "The Cure"
+    },
+    {
+        file: new Audio('./assets/songs/track7.mp3'),
+        cover: "./assets/songs/cover7.jpg",
+        name: "Arabella",
+        artist: "Arctic Monkeys"
     }
 ]
 
@@ -43,11 +73,13 @@ const updateSongInfo = (track_i) => {
 
 const play = () => {
     audio_toggle();
+    audio[track_index].file.volume = 0.1
     audio[track_index].file.play();
     updateSongInfo(track_index);
     audio[track_index].file.onended = function () {
         track_index += 1
         if (track_index >= audio.length) track_index = 0
+        audio[track_index].file.volume = 0.1
         audio[track_index].file.play()
     }
 }
@@ -61,6 +93,7 @@ const next_song = () => {
     audio[track_index].file.pause();
     audio[track_index].file.currentTime = 0;
     track_index = track_index === audio.length - 1 ? 0 : track_index + 1;
+    audio[track_index].file.volume = 0.1
     audio[track_index].file.play();
     updateSongInfo(track_index);
 }
@@ -69,6 +102,7 @@ const previous_song = () => {
     audio[track_index].file.pause();
     audio[track_index].file.currentTime = 0;
     track_index = track_index === 0 ? audio.length - 1 : track_index - 1;
+    audio[track_index].file.volume = 0.1
     audio[track_index].file.play();
     updateSongInfo(track_index);
 }
